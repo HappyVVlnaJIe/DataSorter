7f040cec21b3012c759f0ae11dd848a60906fb04
HappyVVlnaJIe <sitnikov_kp1998@mail.ru>
2025-11-09 14:17:50 +0500

    changed sorting method, added storage of sorted data

--- include/DataStorage.h
+++ include/DataStorage.h
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 #include <regex>
+#include <unordered_map>
+#include <functional>
 
 namespace fs = std::filesystem;
 
@@ -17,13 +19,17 @@ namespace DataSorters {
 		DataStorage(const fs::path& file_path);
 		void sort(const int& sort_type);
 	private:
+		std::vector<int> sort_data(const int& sort_type);
+		void create_sort_types();
+		void output_sorted_data(const int& sort_type);
 		struct Person	{
 			std::wstring second_name;
 			std::wstring name;
 			uint64_t phone;
 		};
 		std::vector<Person> data;
+		std::unordered_map<int, std::vector<int>> sorted_data_by_type;
 		const std::wregex r;
+		std::unordered_map<int, std::function<bool(const int&, const int&)>> sort_types;
 	};
-
 }
--- src/DataStorage.cpp
+++ src/DataStorage.cpp
@@ -1,13 +1,15 @@
 #include "DataStorage.h"
-#include "DataSorter.h"
 
 #include <fstream>
 #include <iostream>
 #include <codecvt>
+#include <numeric>
 
 namespace DataSorters {
 
-	DataStorage::DataStorage(const fs::path& file_path) :r(L"([À-ß][à-ÿ]*)[ ]([À-ß][à-ÿ]*): ([0-9]+)") {
+	DataStorage::DataStorage(const fs::path& file_path) :
+		r(L"([À-ß][à-ÿ]*)[ ]([À-ß][à-ÿ]*): ([0-9]+)")
+	{
 		if (!fs::exists(file_path)) {
 			throw std::exception("Error: 'file doesn't exist'");
 		}
@@ -34,60 +36,66 @@ namespace DataSorters {
 				}
 			}
 		}
+		create_sort_types();
 	}
 
 	void DataStorage::sort(const int& sort_type) {
-		//TODO: remove code duplication
-		if (sort_type == sort_by_name) {
-			DataSorter<std::wstring, std::wstring, uint64_t> data_sorter;
-			for (auto& elem : this->data) {
-				data_sorter.add(elem.name, elem.second_name, elem.phone);
+		if (sort_types.contains(sort_type)) {
+			if (!sorted_data_by_type.contains(sort_type)) {
+				sorted_data_by_type[sort_type] = sort_data(sort_type);
 			}
-			std::wcout << data_sorter;
-		}
-		else if (sort_type == sort_by_second_name) {
-			DataSorter<std::wstring, std::wstring, uint64_t> data_sorter;
-			for (auto& elem : this->data) {
-				data_sorter.add(elem.second_name, elem.name, elem.phone);
-			}
-			std::wcout << data_sorter;
-		}
-		else if (sort_type == sort_by_phone) {
-			DataSorter<uint64_t, std::wstring, std::wstring> data_sorter;
-			for (auto& elem : this->data) {
-				data_sorter.add(elem.phone, elem.name, elem.second_name);
-			}
-			std::wcout << data_sorter;
 		}
 		else {
 			throw std::exception("Error: 'non-existent sort type'");
 		}
-		
-		////TODO: remove code duplication
-		//switch (sort_type)
-		//{
-		//case sort_by_name:
-		//	std::sort(data.begin(), data.end(), [](Person a, Person b) {
-		//		return a.name.compare(b.name);
-		//		});
-		//	break;
-	
-		//case sort_by_second_name:
-		//	std::sort(data.begin(), data.end(), [](Person a, Person b) {
-		//		return a.second_name.compare(b.second_name);
-		//		});
-		//	break;
-
-		//case sort_by_phone:
-		//	std::sort(data.begin(), data.end(), [](Person a, Person b) {
-		//		return a.phone > b.phone;
-		//		});
-		//	break;
-		//default:
-		//	break;
-		//}
-		
+		output_sorted_data(sort_type);
 	}
 
+	std::vector<int> DataStorage::sort_data(const int& sort_type) {
+		std::vector<int> sorted_data;
+		sorted_data.resize(data.size());
+		std::iota(sorted_data.begin(), sorted_data.end(), 0);
+		std::sort(sorted_data.begin(), sorted_data.end(), sort_types[sort_type]);
+		return sorted_data;
+	}
+
+	void DataStorage::create_sort_types() {
+		sort_types[sort_by_name] = [this](int lhs, int rhs) {
+			return data[lhs].name.compare(data[rhs].name) < 0;
+			};
+
+		sort_types[sort_by_second_name] = [this](int lhs, int rhs) {
+			return data[lhs].second_name.compare(data[rhs].second_name) < 0;
+			};
+
+		sort_types[sort_by_phone] = [this](int lhs, int rhs) {
+			return data[lhs].phone < data[rhs].phone;
+			};
+	}
+
+	void DataStorage::output_sorted_data(const int& sort_type) {
+		switch (sort_type)
+		{
+		case sort_by_name:
+			for (auto& indx : sorted_data_by_type[sort_by_name]) {
+				std::wcout << data[indx].name << L" " << data[indx].second_name << L": " << data[indx].phone << std::endl;
+			}
+			break;
+
+		case sort_by_second_name:
+			for (auto& indx : sorted_data_by_type[sort_by_second_name]) {
+				std::wcout << data[indx].second_name << L" " << data[indx].name << L": " << data[indx].phone << std::endl;
+			}
+			break;
+
+		case sort_by_phone:
+			for (auto& indx : sorted_data_by_type[sort_by_phone]) {
+				std::wcout << data[indx].phone << L": " << data[indx].name << L" " << data[indx].second_name << std::endl;
+			}
+			break;
+		default:
+			break;
+		}
+	}
 }
 
